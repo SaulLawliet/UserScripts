543d14fab52142f5a30065f745b598516df25605
Saul Lawliet <october.sunbathe@gmail.com>
2020-01-16 17:48:30 +0800

    new: IMDb页面增强

---
+++ IMDb_Extra_Links/IMDb_Extra_Links.user.js
@@ -0,0 +1,79 @@
+// ==UserScript==
+// @name         IMDb页面增强
+// @description  增强IMDb电影页面，添加豆瓣电影链接和国内正版播放源链接, 以及字幕和torrent
+// @version      0.10
+// @author       Saul Lawliet
+// @namespace    https://github.com/SaulLawliet
+// @homepage     https://github.com/SaulLawliet/UserScripts/tree/master/IMDb_Extra_Links
+// @homepageURL  https://github.com/SaulLawliet/UserScripts/tree/master/IMDb_Extra_Links
+// @downloadURL  https://github.com/SaulLawliet/UserScripts/raw/master/IMDb_Extra_Links/Sync_From_Douban.user.js
+// @updateURL    https://github.com/SaulLawliet/UserScripts/raw/master/IMDb_Extra_Links/Sync_From_Douban.user.js
+// @match        www.imdb.com/title/*
+// @connect      movie.douban.com
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  var imdbID = window.location.href.split("/")[4];
+  GM_xmlhttpRequest({
+    method: "get",
+    url: "https://movie.douban.com/j/subject_suggest?q=" + imdbID,
+    onload: function (result) {
+      var json = JSON.parse(result.responseText)[0];
+      if (json.episode.length > 0) {
+        // TV
+        GM_xmlhttpRequest({
+          method: "get",
+          url: json.url,
+          onload: function (result) {
+            var titleArr = json.title.split(" ");
+            if (titleArr.length > 1) {
+              $(result.responseText).find("#season > option").each(function () {
+                appendBase(imdbID, this.value, titleArr[0] + " 第" + numberToText(this.text) + "季");
+              });
+            } else {
+              appendBase(imdbID, json.id, json.title);
+            }
+          }
+        });
+      } else {
+        // Movie
+        appendBase(imdbID, json.id, json.title);
+
+        GM_xmlhttpRequest({
+          method: "get",
+          url: json.url,
+          onload: function (result) {
+            $(result.responseText).find('.playBtn').each(function () {
+              $('.subtext').append(' | ').append($(this));
+            });
+          }
+        });
+      }
+    }
+  });
+
+  // 数字转换成汉字
+  function numberToText(number) {
+    var text = "";
+    var ones = ["", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
+    var ten = parseInt(number / 10);
+    if (ten > 1) {
+      text += ones[ten];
+    }
+    if (ten > 0) {
+      text += "十";
+    }
+    text += ones[number % 10];
+    return text;
+  }
+
+  function appendBase(imdbID, doubanId, title) {
+    $('.subtext').append('<br>' + title + ' | <a href="https://movie.douban.com/subject/' + doubanId + '" target="_blank">豆瓣电影</a>');
+    $('.subtext').append(' | <a href="https://rarbgprx.org/torrents.php?imdb=' + imdbID + '" target="_blank">RARBG</a>');
+    $('.subtext').append(' | <a href="http://subhd.com/do0/' + doubanId + '" target="_blank">Sub HD</a>');
+  }
+
+})();
