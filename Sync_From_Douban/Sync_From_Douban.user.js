6724bfc401f86d49425f592923f2033aeeae1f1a
Saul Lawliet <october.sunbathe@gmail.com>
2020-01-19 15:56:56 +0800

    update: 从豆瓣同步用户数据

--- Sync_From_Douban/Sync_From_Douban.user.js
+++ Sync_From_Douban/Sync_From_Douban.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         从豆瓣同步用户数据
 // @description  在其他网站展示豆瓣的阅读记录
-// @version      0.1
+// @version      0.2
 // @author       Saul Lawliet
 // @namespace    https://github.com/SaulLawliet
 // @homepage     https://github.com/SaulLawliet/UserScripts/tree/master/Sync_From_Douban
 // @homepageURL  https://github.com/SaulLawliet/UserScripts/tree/master/Sync_From_Douban
 // @downloadURL  https://github.com/SaulLawliet/UserScripts/raw/master/Sync_From_Douban/Sync_From_Douban.user.js
 // @updateURL    https://github.com/SaulLawliet/UserScripts/raw/master/Sync_From_Douban/Sync_From_Douban.user.js
-// @match        https://www.amazon.cn/*
-// @match        https://read.douban.com/*
+// @match        www.amazon.cn/*
+// @match        read.douban.com/*
 // @connect      api.douban.com
 // @grant        GM_xmlhttpRequest
 // @grant        GM_getValue
