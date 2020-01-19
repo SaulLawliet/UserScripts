a6cafb58c320fb999bf21f5626d27df8ce5b0290
Saul Lawliet <october.sunbathe@gmail.com>
2020-01-19 15:56:23 +0800

    update: dst-skins-checklist自动同步数据

--- DST_Skins_Sync/DST_Skins_Sync.user.js
+++ DST_Skins_Sync/DST_Skins_Sync.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         dst-skins-checklist自动同步数据
-// @description  为 https://dst-skins-checklist.github.io/ 自动拉取玩家数据
-// @version      0.1
+// @description  为 dst-skins-checklist.github.io 自动拉取玩家数据
+// @version      0.2
 // @author       Saul Lawliet
 // @namespace    https://github.com/SaulLawliet
 // @homepage     https://github.com/SaulLawliet/UserScripts/tree/master/DST_Skins_Sync
 // @homepageURL  https://github.com/SaulLawliet/UserScripts/tree/master/DST_Skins_Sync
 // @downloadURL  https://github.com/SaulLawliet/UserScripts/raw/master/DST_Skins_Sync/DST_Skins_Sync.user.js
 // @updateURL    https://github.com/SaulLawliet/UserScripts/raw/master/DST_Skins_Sync/DST_Skins_Sync.user.js
-// @match        https://dst-skins-checklist.github.io/
-// @connect      https://steamcommunity.com
+// @match        dst-skins-checklist.github.io
+// @connect      steamcommunity.com
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
