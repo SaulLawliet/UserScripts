417f0b1b0f9447173f8dbce5fa95f6dd42428106
Saul Lawliet <october.sunbathe@gmail.com>
2020-03-01 09:37:32 +0800

    update: Steam 商店页面增加

--- Steam_Store_More_Info/Steam_Store_More_Info.user.js
+++ Steam_Store_More_Info/Steam_Store_More_Info.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam 商店页面增加
-// @description  给 Steam 商店中的游戏页面, 添加更多信息, 如: 中文名, steamDb链接
-// @version      0.1
+// @description  给 Steam 商店中的游戏页面, 添加更多信息, 如: 中文名, steamDb 链接
+// @version      0.2
 // @author       Saul Lawliet
 // @namespace    https://github.com/SaulLawliet
 // @homepage     https://github.com/SaulLawliet/UserScripts/tree/master/Steam_Store_More_Info
@@ -9,7 +9,7 @@
 // @downloadURL  https://github.com/SaulLawliet/UserScripts/raw/master/Steam_Store_More_Info/Steam_Store_More_Info.user.js
 // @updateURL    https://github.com/SaulLawliet/UserScripts/raw/master/Steam_Store_More_Info/Steam_Store_More_Info.user.js
 // @match        store.steampowered.com/app/*
-// @connect      www.douban.com
+// @connect      graphql.xy.huijitrans.com
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
@@ -21,28 +21,38 @@
 
   addSteamDbLink(appId);
 
-  var element = document.getElementsByClassName("apphub_AppName")[0];
+  var appName = document.getElementsByClassName("apphub_AppName")[0].innerText;
 
+  // 修复一下appName的格式
+  appName = appName.replace(/:/g, ": ")
+
+  // https://game.tgbus.com/search?q=NieR%3A%20Automata%E2%84%A2
   GM_xmlhttpRequest({
-    method: "get",
-    url: "https://www.douban.com/j/ilmen/game/search?genres=&platforms=&sort=rating&q=" + element.innerText.replace(/ /g, "+"),
+    method: "POST",
+    url: "https://graphql.xy.huijitrans.com/graphql",
+    data: '{"operationName":null,"variables":{"q":"' + appName + '","first":1,"skip":0},"query":"query SearchResults($q: String\u0021, $first: Int\u0021, $skip: Int\u0021) {\\n  esearch(query: $q, first: $first, skip: $skip) {\\n    gameresults {\\n      game {\\n        id\\n        name\\n        names {\\n          content\\n          lang\\n        }\\n        covers {\\n          path\\n        }\\n        summary_text\\n        first_release_date\\n        companies_developers {\\n          ...CompanyInfo\\n        }\\n        platforms {\\n          ...TagInfo\\n        }\\n        genres {\\n          ...TagInfo\\n        }\\n        player_perspectives {\\n          ...TagInfo\\n        }\\n      }\\n      score\\n    }\\n    total\\n  }\\n}\\n\\nfragment TagInfo on collection {\\n  id\\n  name\\n  names {\\n    alias\\n    en_us\\n    formal\\n    ja_jp\\n    old\\n    short\\n    zh_cn\\n  }\\n}\\n\\nfragment CompanyInfo on companies_collect {\\n  id\\n  name\\n  names {\\n    alias\\n    en_us\\n    formal\\n    ja_jp\\n    old\\n    short\\n    zh_cn\\n  }\\n}\\n"}',
     onload: function (result) {
       var json = JSON.parse(result.responseText);
-      if (json.total > 0) {
-        element.innerText = json.games[0].title;
-      }
+      addChineseName(appName, json.data.esearch.gameresults[0].game.name);
     }
   });
 
+
   function addSteamDbLink(appId) {
-    // var spanNode = document.createElement("span");
-    // spanNode.innter
     var aNode = document.createElement("a");
     aNode.className = "btnv6_blue_hoverfade btn_medium";
     aNode.href = "https://steamdb.info/app/" + appId;
     aNode.innerHTML = "<span>SteamDb</span>";
 
     document.getElementsByClassName("apphub_OtherSiteInfo")[0].appendChild(aNode);
+  }
+
+  function addChineseName(appName, name) {
+    var aNode = document.createElement("a");
+    aNode.href = "https://game.tgbus.com/search?q=" + appName;
+    aNode.innerText = name;
+
+    document.getElementsByClassName("apphub_OtherSiteInfo")[0].prepend(aNode);
   }
 
 })();
