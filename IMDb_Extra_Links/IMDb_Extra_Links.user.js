2b2693e2c0021864abd28a39b828349f4e8522a1
SaulLawliet <october.sunbathe@gmail.com>
2022-03-23 11:51:04 +0800

    imdb: 适配最新UI

--- IMDb_Extra_Links/IMDb_Extra_Links.user.js
+++ IMDb_Extra_Links/IMDb_Extra_Links.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         IMDb 电影页面增强
-// @description  增强 IMDb 电影页面，添加豆瓣电影链接和国内正版播放源链接, 以及字幕和torrent
-// @version      0.10
+// @name         IMDb电影页面增强
+// @description  增强IMDb电影页面，添加豆瓣电影链接和国内正版播放源链接, 以及字幕和torrent
+// @version      0.11
 // @author       Saul Lawliet
 // @namespace    https://github.com/SaulLawliet
 // @homepage     https://github.com/SaulLawliet/UserScripts/tree/master/IMDb_Extra_Links
@@ -11,7 +11,11 @@
 // @match        www.imdb.com/title/*
 // @connect      movie.douban.com
 // @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @require http://code.jquery.com/jquery-3.6.0.min.js
 // ==/UserScript==
+
+GM_addStyle(".extra_list a { color: lightgray; }");
 
 (function () {
   'use strict';
@@ -47,7 +51,7 @@
           url: json.url,
           onload: function (result) {
             $(result.responseText).find('.playBtn').each(function () {
-              $('.subtext').append(' | ').append($(this));
+              $('#extra_list').append(' | ').append($(this));
             });
           }
         });
@@ -71,9 +75,14 @@
   }
 
   function appendBase(imdbID, doubanId, title) {
-    $('.subtext').append('<br>' + title + ' | <a href="https://movie.douban.com/subject/' + doubanId + '" target="_blank">豆瓣电影</a>');
-    $('.subtext').append(' | <a href="https://rarbgprx.org/torrents.php?imdb=' + imdbID + '" target="_blank">RARBG</a>');
-    $('.subtext').append(' | <a href="http://subhd.com/do0/' + doubanId + '" target="_blank">Sub HD</a>');
+      const ul = document.getElementsByClassName('ipc-inline-list')[0];
+      const li1 = document.createElement('li');
+      li1.className = "ipc-inline-list__item extra_list";
+      li1.innerHTML = title
+      li1.innerHTML += ' | <a href="https://movie.douban.com/subject/' + doubanId + '" target="_blank">豆瓣电影</a>';
+      li1.innerHTML += ' | <a href="https://rarbgprx.org/torrents.php?imdb=' + imdbID + '" target="_blank">RARBG</a>';
+      li1.innerHTML += ' | <a href="http://subhd.com/do0/' + doubanId + '" target="_blank">Sub HD</a>';
+      ul.appendChild(li1);
   }
 
 })();
