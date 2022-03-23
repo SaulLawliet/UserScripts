968fcbb5675935f213c72a25d8f6f534df1920bf
SaulLawliet <october.sunbathe@gmail.com>
2022-03-23 14:10:07 +0800

    This is a basic implementation that works.

--- Key_Joker_Sync/Key_Joker_Sync.user.js
+++ Key_Joker_Sync/Key_Joker_Sync.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KeyJoker 标记已有游戏
 // @description  KeyJoker 标记已有游戏
-// @version      0.2
+// @version      0.3
 // @author       Saul Lawliet
 // @namespace    https://github.com/SaulLawliet
 // @homepage     https://github.com/SaulLawliet/UserScripts/tree/master/Key_Joker_Sync
@@ -12,6 +12,7 @@
 // @connect      steamdb.keylol.com
 // @connect      api.steampowered.com
 // @grant        GM_xmlhttpRequest
+// @require      https://cdn.jsdelivr.net/gh/SaulLawliet/UserScripts@8baff43/snippets.js
 // ==/UserScript==
 
 (function () {
@@ -22,21 +23,18 @@
   const pathname = document.location.pathname;
   console.log(pathname);
   if (pathname == '/' || pathname == '/bulkbuy' || pathname.startsWith("/giveways")) {
-    GM_xmlhttpRequest({
-      method: 'get',
-      url: 'https://steamdb.keylol.com/syncProxy.php?type=own&id=' + steamId,
-      onload: function (result) {
-        const data = JSON.parse(result.responseText)
-        console.log('game_count: ' + data.response.game_count)
-        const appidList = data.response.games.map(x => x.appid)
+    getAppidListByApi(steamId).then((data) => {
+      console.log('game_count: ' + data.response.game_count)
+      const appidList = data.response.games.map(x => x.appid)
 
-        if (pathname.startsWith("/giveaways")) {
-          const appid = parseInt(pathname.split('-').pop())
-          if (appidList.includes(appid)) {
-            $('.border-bottom')[0].style = "background-color: yellowgreen;";
-          }
-        } else {
-          setInterval(() => {() =>
+      if (pathname.startsWith("/giveaways")) {
+        const appid = parseInt(pathname.split('-').pop())
+        if (appidList.includes(appid)) {
+          $('.border-bottom')[0].style = "background-color: yellowgreen;";
+        }
+      } else {
+        setInterval(() => {
+          () =>
             $('a').each((index, element) => {
               if (element.href.startsWith("https://www.keyjoker.com/giveaways/")) {
                 element.parentNode.parentNode.firstChild.style = "";
@@ -46,26 +44,8 @@
                 }
               }
             })
-          }, 3000);
-        }
+        }, 3000);
       }
-    });
-  }
-
-  function getAppidListByApi() {
-    // let url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=1470922A6B6E5C001546E51ACA5D987B&include_played_free_games=true&steamid=${steamId}`;
-    let url = `https://steamdb.keylol.com/syncProxy.php?type=own&id=${steamId}`;
-    return new Promise((resolve, reject) => {
-      GM_xmlhttpRequest({
-        method: 'get',
-        url: url,
-        responseType: 'json',
-        onload: function (result) {
-          resolve(result.response);
-        },
-        onerror: reject,
-        ontimeout: reject,
-      })
     });
   }
 })();
