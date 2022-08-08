1d2dd40889921fa4a4005c922f0a513ceb55c416
SaulLawliet <october.sunbathe@gmail.com>
2022-08-08 10:55:14 +0800

    MelvorIdle_Expansion: fix logic order

--- MelvorIdle_Expansion/MelvorIdle_Expansion.user.js
+++ MelvorIdle_Expansion/MelvorIdle_Expansion.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Melvor Idle: Expansion
-// @description  Melvor Idle: Expansion some function. Like: astrologyPerfectAlways, slayerTaskCostFree, masteryXPToPoolOverflow, etc.
-// @version      0.2
+// @description  Melvor Idle: Expansion some function. Like: masteryXPToPoolOverflow, astrologyPerfectAlways, slayerTaskCostFree, unlimitedOffline etc.
+// @version      0.3
 // @author       Saul Lawliet
 // @namespace    https://github.com/SaulLawliet
 // @homepage     https://github.com/SaulLawliet/UserScripts/tree/master/MelvorIdle_Expansion
@@ -12,12 +12,21 @@
 // @match        https://melvoridle.com/*
 // ==/UserScript==
 
+// base on: https://greasyfork.org/en/scripts/435372-melvor-unlimited-offline
 ((main) => {
-    const script = document.createElement("script");
+    const script = document.createElement('script');
     script.textContent = `try { (${main})(); } catch (e) { console.log(e); }`;
     document.body.appendChild(script).parentNode.removeChild(script);
 })(() => {
-    function Script() {
+    setTimeout(() => {
+        function patchCode(code, match, replacement) {
+            const codeString = code
+                .toString()
+                .replace(match, replacement)
+                .replace(/^function (\w+)/, "window.$1 = function");
+            eval(codeString);
+        }
+
         const SCRIPT_NAME = "Expansion";
 
         // TODO: setting UI.
@@ -28,50 +37,28 @@
             unlimitedOffline: true,
         };
 
-        // copy from: https://greasyfork.org/en/scripts/435372-melvor-unlimited-offline
-        function patchCode(func, match, replacement) {
-            const codeString = func
-                .toString()
-                .replace(match, replacement)
-                .replace(/^function (\w+)/, "window.$1 = function");
-            eval(codeString);
+        if (config.astrologyPerfectAlways && Astrology && Astrology.modifierMagnitudeChances) {
+            Astrology.modifierMagnitudeChances = [0, 0, 0, 0, 100];
         }
 
-        (() => {
-            if (config.astrologyPerfectAlways) {
-                Astrology.modifierMagnitudeChances = [0, 0, 0, 0, 100];
-            }
-
-            if (config.slayerTaskCostFree) {
-                SlayerTask.data.forEach((task) => {
-                    task.cost = 0;
-                });
-            }
-
-            if (config.masteryXPToPoolOverflow) {
-                patchCode(
-                    addMasteryXPToPool,
-                    /(MASTERY\[skill\].pool>)/,
-                    "false&&$1"
-                );
-            }
-
-            if (config.unlimitedOffline) {
-                patchCode(getOfflineTimeDiff, 64800000, "Infinity");
-            }
-
-            console.log(`${SCRIPT_NAME} loaded.`);
-        })();
-    }
-
-    function loadScript() {
-        if (typeof isLoaded !== typeof undefined && isLoaded) {
-            // Only load script after game has opened
-            clearInterval(scriptLoader);
-
-            Script();
+        if (config.slayerTaskCostFree && SlayerTask && SlayerTask.data) {
+            SlayerTask.data.forEach((task) => {
+                task.cost = 0;
+            });
         }
-    }
 
-    const scriptLoader = setInterval(loadScript, 200);
+        if (config.masteryXPToPoolOverflow) {
+            patchCode(
+                addMasteryXPToPool,
+                /(MASTERY\[skill\].pool>)/,
+                "false&&$1"
+            );
+        }
+
+        if (config.unlimitedOffline) {
+            patchCode(getOfflineTimeDiff, 64800000, "Infinity");
+        }
+
+        console.log(`${SCRIPT_NAME} loaded.`);
+    }, 100);
 });
