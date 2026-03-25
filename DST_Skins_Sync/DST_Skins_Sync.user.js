875bfbb7db7e0603b03393e0d8551d4e6d352382
SaulLawliet <october.sunbathe@gmail.com>
2026-03-25 16:22:44 +0800

    DST_Skins_Sync: 添加配置按钮，可以配置steamId

--- DST_Skins_Sync/DST_Skins_Sync.user.js
+++ DST_Skins_Sync/DST_Skins_Sync.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         dst-skins-checklist 自动同步数据
 // @description  为 dst-skins-checklist.github.io 自动拉取玩家数据
-// @version      0.3
+// @version      0.4
 // @author       Saul Lawliet
 // @namespace    https://github.com/SaulLawliet
 // @homepage     https://github.com/SaulLawliet/UserScripts/tree/master/DST_Skins_Sync
@@ -11,38 +11,51 @@
 // @match        dst-skins-checklist.github.io
 // @connect      steamcommunity.com
 // @grant        GM_xmlhttpRequest
+// @grant        GM_getValue
+// @grant        GM_setValue
 // ==/UserScript==
 
 (function () {
   "use strict";
 
-  var steamId = "76561198130053503";
+  let btn = document.createElement("button");
+  btn.innerHTML = "设置 Steam ID";
+  btn.style = "position:fixed;top:20px;right:20px;z-index:9999;font-size:medium;";
+  document.body.appendChild(btn);
+
+  btn.onclick = function () {
+    let val = window.prompt("你之前的ID是【" + GM_getValue("steamId", "") + "】，请输入：");
+    if (val) GM_setValue("steamId", val);
+  };
+
+  let steamId = GM_getValue("steamId", null);
   console.log("steamId:", steamId);
 
-  // 只考虑不足2000个的情况
-  GM_xmlhttpRequest({
-    method: "get",
-    url: "https://steamcommunity.com/inventory/" + steamId + "/322330/1?count=2000",
-    onload: function (result) {
-      document.getElementById("jsonNew").value = result.responseText;
-      resolveNew();
-      exitModal();
+  if (steamId != null) {
+    GM_xmlhttpRequest({
+      method: "get",
+      url: "https://steamcommunity.com/inventory/" + steamId + "/322330/1?count=2000",
+      onload: function (result) {
+        document.getElementById("jsonNew").value = result.responseText;
+        resolveNew();
+        exitModal();
 
-      var data = JSON.parse(result.responseText);
-      let lastAssetId = data.last_assetid;
-      console.log("lastAssetId:", lastAssetId);
-      if (lastAssetId !== undefined) {
-        GM_xmlhttpRequest({
-          method: "get",
-          url: "https://steamcommunity.com/inventory/" + steamId + "/322330/1?start_assetid=" + lastAssetId + "&count=2000",
-          onload: function (result2) {
-            document.getElementById("jsonNew2").value = result2.responseText;
-            resolveNew(1);
-            exitModal();
-            console.log("成功");
-          },
-        });
-      }
-    },
-  });
+        let data = JSON.parse(result.responseText);
+        let lastAssetId = data.last_assetid;
+        console.log("lastAssetId:", lastAssetId);
+        if (lastAssetId !== undefined) {
+          GM_xmlhttpRequest({
+            method: "get",
+            url: "https://steamcommunity.com/inventory/" + steamId + "/322330/1?start_assetid=" + lastAssetId + "&count=2000",
+            onload: function (result2) {
+              document.getElementById("jsonNew2").value = result2.responseText;
+              resolveNew(1);
+              exitModal();
+              console.log("成功");
+            },
+          });
+        }
+      },
+    });
+  }
 })();
